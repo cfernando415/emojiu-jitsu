e0e5354eafbcb7cc3a1ec7c8dfc70eeab8f9950a
cfernando415 <ca.fernando415@gmail.com>
2019-03-24 23:35:16 -0400

    added search function

--- js/emoji.js
+++ js/emoji.js
@@ -1,18 +1,36 @@
+/* eslint-disable no-console */
+/* eslint-disable no-debugger */
+let allEmoji = [];
+
 document.addEventListener("DOMContentLoaded", () => {
   fetch("http://localhost:3000/emoji")
     .then(res => res.json())
     .then(data => defineEmoji(data));
 
   document.getElementById("submit").addEventListener("click", submitHandler);
+  document
+    .getElementById("next-button")
+    .addEventListener("click", clickHandler);
+
+  document.getElementById("search").addEventListener("click", searchHandler);
 });
 
 function defineEmoji(emoji) {
-  const pEmoji = document.getElementById("emoji");
+  allEmoji.push(...emoji);
+  const pEmoji = document.getElementById("title");
+  const emojiName = document.createElement("small");
+  const spanEmoji = document.createElement("span");
 
-  randEmoji = emoji[Math.floor(Math.random() * emoji.length)];
+  const randEmoji = emoji[Math.floor(Math.random() * emoji.length)];
 
-  pEmoji.dataset.id = randEmoji.id;
-  pEmoji.innerText = randEmoji.image;
+  emojiName.innerText = randEmoji.name;
+  spanEmoji.innerText = randEmoji.image;
+  spanEmoji.setAttribute("id", "emoji");
+  spanEmoji.setAttribute("role", "img");
+  spanEmoji.setAttribute("alt", `${randEmoji.name}`);
+  spanEmoji.dataset.id = randEmoji.id;
+  pEmoji.append(emojiName);
+  pEmoji.append(spanEmoji);
 
   listDefinitions(randEmoji.definitions);
 }
@@ -21,24 +39,24 @@ function listDefinitions(definitions) {
   const ulEmojiDefinition = document.getElementById("description");
   ulEmojiDefinition.innerHTML = "";
 
-  for (word of definitions) {
+  for (let word of definitions) {
     const ulChild = document.createElement("li");
     ulChild.innerText = word;
     ulEmojiDefinition.append(ulChild);
   }
 }
 
-function submitHandler(e) {
+function submitHandler() {
   const inputField = document.querySelector("input");
   const definitions = [inputField.value];
   const ulNode = document.getElementById("description");
-  const pEmoji = document.getElementById("emoji");
+  const spanTagEmoji = document.getElementById("emoji");
 
-  for (node of ulNode.childNodes) {
+  for (let node of ulNode.childNodes) {
     definitions.push(node.innerText);
   }
 
-  fetch(`http://localhost:3000/emoji/${parseInt(pEmoji.dataset.id)}`, {
+  fetch(`http://localhost:3000/emoji/${parseInt(spanTagEmoji.dataset.id)}`, {
     method: "PATCH",
     headers: { "Content-Type": "application/json", accept: "application/json" },
     body: JSON.stringify({ definitions: definitions })
@@ -46,5 +64,64 @@ function submitHandler(e) {
 
   listDefinitions(definitions);
 
+  allEmoji.forEach(emoji => {
+    if (emoji.id === parseInt(spanTagEmoji.dataset.id))
+      emoji.definitions.push(inputField.value);
+  });
+
   inputField.value = "";
+}
+
+function clickHandler() {
+  // console.log("click");
+  window.location.reload();
+}
+
+function searchHandler() {
+  const q = document.getElementById("searchField").value;
+
+  const searchResults = allEmoji.filter(emoji =>
+    emoji.name.toLowerCase().includes(q.toLowerCase())
+  );
+
+  const divSearch = document.getElementById("search-results");
+
+  divSearch.innerHTML = "";
+
+  for (let result of searchResults) {
+    const pTitle = document.createElement("p");
+    const smallTagResult = document.createElement("small");
+    const spanTagResult = document.createElement("span");
+    const pRelabel = document.createElement("p");
+    const smallTagRelabel = document.createElement("small");
+    const spanCount = document.createElement("span");
+    const ulNode = document.createElement("ul");
+
+    pTitle.setAttribute("class", "title");
+    smallTagResult.innerText = result.name;
+    spanTagResult.setAttribute("class", "emoji");
+    spanTagResult.setAttribute("role", "img");
+    spanTagResult.setAttribute("alt", result.name);
+    spanTagResult.innerText = result.image;
+
+    pTitle.append(smallTagResult);
+    pTitle.append(spanTagResult);
+
+    smallTagRelabel.innerText = `This emoji has been relabeled ${
+      result.definitions.length
+    } time(s) as:`;
+
+    pRelabel.append(smallTagRelabel);
+    pRelabel.append(spanCount);
+
+    for (let definition of result.definitions) {
+      const liNode = document.createElement("li");
+      liNode.innerText = definition;
+      ulNode.append(liNode);
+    }
+
+    divSearch.append(pTitle);
+    divSearch.append(pRelabel);
+    divSearch.append(ulNode);
+  }
 }
--- js/index.js
+++ js/index.js
@@ -1,93 +1,104 @@
 function handleCommand(event) {
-    // Handles both mouse clicks and keyboard
-    // activate with Enter or Space
+  // Handles both mouse clicks and keyboard
+  // activate with Enter or Space
 
-    // Get the new name value from the input element
-    let newNameInput = document.getElementById('newName');
-    let name = newNameInput.value;
-    newNameInput.value = ''; // clear the text field
-    newNameInput.focus();  // give the text field focus to enable entering and additional name.
+  // Get the new name value from the input element
+  let newNameInput = document.getElementById("newName");
+  let name = newNameInput.value;
+  newNameInput.value = ""; // clear the text field
+  newNameInput.focus(); // give the text field focus to enable entering and additional name.
 
-    // Don't add blank entries to the list.
-    if(name.length > 0) {
-        listItem = document.createElement('li');
-        listItem.appendChild(document.createTextNode(name));
+  // Don't add blank entries to the list.
+  if (name.length > 0) {
+    listItem = document.createElement("li");
+    listItem.appendChild(document.createTextNode(name));
 
-        // Add the new name to the list.
-        let list = document.getElementById('nameList');
-        list.appendChild(listItem);
-    }
+    // Add the new name to the list.
+    let list = document.getElementById("nameList");
+    list.appendChild(listItem);
+  }
 }
 (function() {
   // Get relevant elements and collections
-  const tabbed = document.querySelector('.tabbed');
-  const tablist = tabbed.querySelector('ul');
-  const tabs = tablist.querySelectorAll('a');
+  const tabbed = document.querySelector(".tabbed");
+  const tablist = tabbed.querySelector("ul");
+  const tabs = tablist.querySelectorAll("a");
   const panels = tabbed.querySelectorAll('[id^="section"]');
-  
+
   // The tab switching function
   const switchTab = (oldTab, newTab) => {
     newTab.focus();
     // Make the active tab focusable by the user (Tab key)
-    newTab.removeAttribute('tabindex');
+    newTab.removeAttribute("tabindex");
     // Set the selected state
-    newTab.setAttribute('aria-selected', 'true');
-    oldTab.removeAttribute('aria-selected');
-    oldTab.setAttribute('tabindex', '-1');
+    newTab.setAttribute("aria-selected", "true");
+    oldTab.removeAttribute("aria-selected");
+    oldTab.setAttribute("tabindex", "-1");
     // Get the indices of the new and old tabs to find the correct
     // tab panels to show and hide
     let index = Array.prototype.indexOf.call(tabs, newTab);
     let oldIndex = Array.prototype.indexOf.call(tabs, oldTab);
     panels[oldIndex].hidden = true;
     panels[index].hidden = false;
-  }
-  
+  };
+
   // Add the tablist role to the first <ul> in the .tabbed container
-  tablist.setAttribute('role', 'tablist');
-  
+  tablist.setAttribute("role", "tablist");
+
   // Add semantics are remove user focusability for each tab
   Array.prototype.forEach.call(tabs, (tab, i) => {
-    tab.setAttribute('role', 'tab');
-    tab.setAttribute('id', 'tab' + (i + 1));
-    tab.setAttribute('tabindex', '-1');
-    tab.parentNode.setAttribute('role', 'presentation');
-    
+    tab.setAttribute("role", "tab");
+    tab.setAttribute("id", "tab" + (i + 1));
+    tab.setAttribute("tabindex", "-1");
+    tab.parentNode.setAttribute("role", "presentation");
+
     // Handle clicking of tabs for mouse users
-    tab.addEventListener('click', e => {
+    tab.addEventListener("click", e => {
       e.preventDefault();
-      let currentTab = tablist.querySelector('[aria-selected]');
+      let currentTab = tablist.querySelector("[aria-selected]");
       if (e.currentTarget !== currentTab) {
         switchTab(currentTab, e.currentTarget);
       }
     });
-    
+
     // Handle keydown events for keyboard users
-    tab.addEventListener('keydown', e => {
+    tab.addEventListener("keydown", e => {
       // Get the index of the current tab in the tabs node list
       let index = Array.prototype.indexOf.call(tabs, e.currentTarget);
       // Work out which key the user is pressing and
       // Calculate the new tab's index where appropriate
-      let dir = e.which === 37 ? index - 1 : e.which === 39 ? index + 1 : e.which === 40 ? 'down' : null;
+      let dir =
+        e.which === 37
+          ? index - 1
+          : e.which === 39
+          ? index + 1
+          : e.which === 40
+          ? "down"
+          : null;
       if (dir !== null) {
         e.preventDefault();
         // If the down key is pressed, move focus to the open panel,
         // otherwise switch to the adjacent tab
-        dir === 'down' ? panels[i].focus() : tabs[dir] ? switchTab(e.currentTarget, tabs[dir]) : void 0;
+        dir === "down"
+          ? panels[i].focus()
+          : tabs[dir]
+          ? switchTab(e.currentTarget, tabs[dir])
+          : void 0;
       }
     });
   });
-  
+
   // Add tab panel semantics and hide them all
   Array.prototype.forEach.call(panels, (panel, i) => {
-    panel.setAttribute('role', 'tabpanel');
-    panel.setAttribute('tabindex', '-1');
-    let id = panel.getAttribute('id');
-    panel.setAttribute('aria-labelledby', tabs[i].id);
-    panel.hidden = true; 
+    panel.setAttribute("role", "tabpanel");
+    panel.setAttribute("tabindex", "-1");
+    let id = panel.getAttribute("id");
+    panel.setAttribute("aria-labelledby", tabs[i].id);
+    panel.hidden = true;
   });
-  
+
   // Initially activate the first tab and reveal the first tab panel
-  tabs[0].removeAttribute('tabindex');
-  tabs[0].setAttribute('aria-selected', 'true');
+  tabs[0].removeAttribute("tabindex");
+  tabs[0].setAttribute("aria-selected", "true");
   panels[0].hidden = false;
 })();
